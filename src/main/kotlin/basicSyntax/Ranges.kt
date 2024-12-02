b40b142db85840c318164c775888fe79ed0be7ea
Krishigina <krishigina@gmial.com>
2024-12-03 00:29:14 +0300

    Ranges

---
+++ src/main/kotlin/basicSyntax/Ranges.kt
@@ -0,0 +1,10 @@
+package org.example.basicSyntax
+
+fun main() {
+    val start = readln().toInt()
+    val end = readln().toInt()
+    val numbers = start..end step 8
+    for (number in numbers){
+        println(number)
+    }
+}

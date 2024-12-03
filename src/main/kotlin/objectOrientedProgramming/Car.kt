906995c121088df8af37081e6af4604d35c95b95
Krishigina <krishigina@gmial.com>
2024-12-03 22:29:58 +0300

    the this keyword and named parameters

--- src/main/kotlin/objectOrientedProgramming/Car.kt
+++ src/main/kotlin/objectOrientedProgramming/Car.kt
@@ -1,13 +1,29 @@
 package org.example.objectOrientedProgramming
 
 class Car {
-    var brand: String = ""
-    var model: String = ""
-    var enginePower: Int = 0
-    var bodyColor:String = ""
+    var brand = ""
+    var model = ""
+    var enginePower = 0
+    var bodyColor = ""
 
-    fun drive(){
-        println("Еду на автомобиле")
+    //добавьте метод init()
+
+    fun drive(enginePower: Int, brand: String, model: String) {
+        if (enginePower >= 120) println("Еду быстро, но недалеко на $brand $model")
+        else println("Еду далеко, но небыстро на $brand $model")
     }
 
+    fun init(brand: String, model: String, enginePower: Int, bodyColor: String){
+        this.brand = brand
+        this.model = model
+        this.enginePower = enginePower
+        this.bodyColor = bodyColor
+    }
+}
+
+fun main() {
+    val car = Car()
+    val data = readln().split(" ")
+    car.init(brand = data[0], model = data[1], enginePower = data[2].toInt(), bodyColor = data[3])
+    car.drive(enginePower = data[2].toInt(), brand = data[0], model = data[1])
 }

f02a8e27d2e319416b0415899ea1c55a1657ebf5
Krishigina <krishigina@gmial.com>
2024-12-03 22:49:51 +0300

    introduction to constructors

--- src/main/kotlin/objectOrientedProgramming/Car.kt
+++ src/main/kotlin/objectOrientedProgramming/Car.kt
@@ -1,29 +1,28 @@
 package org.example.objectOrientedProgramming
 
 class Car {
-    var brand = ""
-    var model = ""
-    var enginePower = 0
-    var bodyColor = ""
+    //сделайте все свойства неизменяемыми
+    val brand: String
+    val model: String
+    val enginePower: Int
+    val bodyColor: String
 
-    //добавьте метод init()
-
-    fun drive() {
-        if (enginePower >= 120) println("Еду быстро, но недалеко на $brand $model")
-        else println("Еду далеко, но небыстро на $brand $model")
-    }
-
-    fun init(brand: String, model: String, enginePower: Int, bodyColor: String){
+    constructor(brand: String, model: String, enginePower: Int, bodyColor: String) {
         this.brand = brand
         this.model = model
         this.enginePower = enginePower
         this.bodyColor = bodyColor
     }
+
+    fun printInfo(){
+        println("$brand $model $enginePower л.с. $bodyColor")
+    }
+
 }
 
 fun main() {
-    val car = Car()
     val data = readln().split(" ")
-    car.init(brand = data[0], model = data[1], enginePower = data[2].toInt(), bodyColor = data[3])
-    car.drive()
+    val car = Car(data[0], data[1], data[2].toInt(), data[3])
+    car.printInfo()
+
 }

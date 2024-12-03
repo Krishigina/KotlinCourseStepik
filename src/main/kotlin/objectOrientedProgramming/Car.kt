f9629fae94db8a6af127494d96bb94a83fe13eea
Krishigina <krishigina@gmial.com>
2024-12-03 18:45:25 +0300

    creating a class and an object

---
+++ src/main/kotlin/objectOrientedProgramming/Car.kt
@@ -0,0 +1,9 @@
+package org.example.objectOrientedProgramming
+
+class Car {
+    var brand: String = ""
+    var model: String = ""
+    var enginePower: Int = 0
+    var bodyColor:String = ""
+
+}

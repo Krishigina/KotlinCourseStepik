7ae99db28ed60342983796bc2ccab43967f59a6e
Krishigina <krishigina@gmial.com>
2025-02-06 21:04:09 +0300

    Observer. Введение

---
+++ src/main/kotlin/multithreading/observer/UserRepository.kt
@@ -0,0 +1,40 @@
+package org.example.multithreading.observer
+
+// Хранилище пользователей
+class UserRepository {
+    private val users = mutableListOf<String>()
+
+    private val observers = mutableListOf<UserLogger>()
+
+    fun addUser(user: String) {
+        users.add(user)
+        notifyObservers()
+    }
+
+    fun removeUser(user: String) {
+        users.remove(user)
+        notifyObservers()
+    }
+
+    fun subscribe(logger: UserLogger){
+        observers.add(logger)
+        logger.onUsersChanged(users)
+    }
+
+    fun unsubscribe(logger: UserLogger){
+        observers.remove(logger)
+    }
+
+    private fun notifyObservers(){
+        for (observer in observers){
+            observer.onUsersChanged(users)
+        }
+    }
+}
+
+// Класс наблюдателя, который подписывается на изменения в UserRepository
+class UserLogger {
+    fun onUsersChanged(users: List<String>){
+        println("[LOG] Пользователи обновлены: $users")
+    }
+}
